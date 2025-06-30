98e02c2d440c29b7d46aaa11f3c65f62e2fbe794
jalalakbar47 <jalalakbarbjr@gmail.com>
2025-06-30 16:51:13 +0500

    Create radar_system.ino
    Initial commit of radar_system.ino file using ultrasonic sensor and servo motor to detect distance and simulate radar motion.

---
+++ radar_system.ino
@@ -0,0 +1,44 @@
+#include <Servo.h>
+
+Servo radarServo;
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+
+void setup() {
+  radarServo.attach(11);  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int angle = 15; angle <= 165; angle++) {
+    radarServo.write(angle);
+    delay(50);
+    distance = getDistance();
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.println(distance);
+  }
+
+  for (int angle = 165; angle >= 15; angle--) {
+    radarServo.write(angle);
+    delay(50);
+    distance = getDistance();
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.println(distance);
+  }
+}
+
+int getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  return duration * 0.034 / 2;
+}
